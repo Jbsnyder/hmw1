a799c6310f92497ef45ed4f8bb1bafe0a7699d4d
Jbsnyder <john.snyder@kellogg.northwestern.edu>
2022-01-25 03:19:02 +0000

    k

--- kmdb.sql
+++ kmdb.sql
@@ -68,13 +68,105 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS roles;
 
 -- Create new tables, according to your domain model
 -- TODO!
 
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  release_year TEXT,
+  mpaa_rating TEXT
+  );
+
+CREATE TABLE persons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT
+    );
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+ movie_id INTEGER,
+ persons_id INTEGER,
+ character_name TEXT,
+ role TEXT
+ );
+
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+Insert into movies (title, release_year, mpaa_rating)
+values  ('Batman Begins', '2005', 'PG-13');
+Insert into movies (title, release_year, mpaa_rating)
+values  ('The Dark Knight', '2008', 'PG-13');
+Insert into movies (title, release_year, mpaa_rating)
+values  ('The Dark Knight Rises', '2012', 'PG-13');
+
+Insert into persons (first_name, last_name)
+values  ('Christian', 'Bale');
+Insert into persons (first_name, last_name)
+values  ('Michael', 'Caine');
+Insert into persons (first_name, last_name)
+values  ('Liam', 'Neeson');
+Insert into persons (first_name, last_name)
+values  ('Katie', 'Holmes');
+Insert into persons (first_name, last_name)
+values  ('Gary', 'Oldman');
+Insert into persons (first_name, last_name)
+values  ('Heath', 'Ledger');
+Insert into persons (first_name, last_name)
+values  ('Aaron', 'Eckhart');
+Insert into persons (first_name, last_name)
+values  ('Maggie', 'Gyllenhaal');
+Insert into persons (first_name, last_name)
+values  ('Tom', 'Hardy');
+Insert into persons (first_name, last_name)
+values  ('Joseph', 'Gordon-Levitt');
+Insert into persons (first_name, last_name)
+values  ('Anne', 'Hathaway');
+Insert into persons (first_name, last_name)
+values  ('Christopher', 'Nolan');
+
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 1, 'Bruce Wayne', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 2, 'Alfred', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 3, "Ra's Al Ghul", 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 4, 'Rachel Dawes', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 5, 'Commissioner Gordon', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (1, 12, '', 'director');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 1, 'Bruce Wayne', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 6, 'Joker', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 7, 'Harvey Dent', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 2, 'Alfred', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 8, 'Rachel Dawes', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (2, 12, '', 'director');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 1, 'Bruce Wayne', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 5, 'Commissioner Gordon', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 9, 'Bane', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 10, 'John Blake', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 11, 'Selina Kyle', 'actor');
+Insert into roles (movie_id, persons_id, character_name, role)
+values  (3, 12, '', 'director');
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -83,6 +175,12 @@
 
 -- The SQL statement for the movies output
 -- TODO!
+
+SELECT movies.title, movies.release_year, movies.mpaa_rating, persons.first_name, persons.last_name
+FROM movies
+INNER JOIN roles ON movies.id = roles.movie_id
+INNER JOIN persons ON persons.id = roles.persons_id
+WHERE roles.role = "director";
 
 -- Prints a header for the cast output
 .print ""
@@ -93,3 +191,9 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+
+SELECT movies.title, persons.first_name, persons.last_name, roles.character_name
+from movies
+INNER Join roles ON movies.id = roles.movie_id
+INNER JOIN persons ON persons.id = roles.persons_id
+WHERE roles.role = "actor";
